d85740bf45351e4bc398f2aca7065528087a57af
Alex Vito <alexvito@Alexs-MacBook-Pro.local>
2020-04-05 11:29:55 -0700

    class 2 changes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { Message } from '../models/message';
+import { SharedService } from '../services/shared.service';
+import { DataService } from '../services/data.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  message: Message = new Message();
+
+  constructor( private shared: SharedService, private data: DataService) {}
+
+  onPost() {
+    this.message.from = this.shared.userName;
+    this.message.createdOn = new Date();
+    
+    this.data.saveMessage(this.message);
+
+    // clear form
+    this.message = new Message();
+  }
 
 }
